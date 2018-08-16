098fd8e9a7bfe85d345ef49f708a3e48d2614b35
Rodrigo Alfonso <rodri042@gmail.com>
2018-08-16 03:41:19 -0300

    Adding compilation specs

--- spec/test_compilation_spec.rb
+++ spec/test_compilation_spec.rb
@@ -1,49 +1,37 @@
-# require_relative './spec_helper'
-#
-# describe HaskellTestHook do
-#   let(:hook) { HaskellTestHook.new(nil) }
-#   let(:compilation) { hook.compile_file_content(treq(sample_content, sample_test, '')) }
-#
-#   let(:sample_content) { 'x = True' }
-#   let(:sample_test) {
-#     <<HASKELL
-# describe "x" $ do
-#   it "should be True" $ do
-#     x `shouldBe` True
-# describe y" $ do
-#   it "should be False" $ do
-#     x `shouldBe` False
-# HASKELL
-#   }
-#   let(:expected_compilation_imports) do
-#     <<HASKELL
-# {-# OPTIONS_GHC -fdefer-type-errors #-}
-# import Test.Hspec
-# import Test.Hspec.Runner (hspecWith, defaultConfig, Config (configFormatter))
-# import Test.QuickCheck
-# import Test.Hspec.Formatters
-#
-# import qualified Control.Exception as Exception
-#
-# import Data.List
-# HASKELL
-#   end
-#   let(:expected_compilation_body) do
-#     <<HASKELL
-# x = True
-#
-# main :: IO ()
-# main = hspecWith defaultConfig {configFormatter = Just structured} $ do
-#     describe "x" $ do
-#       it "should be True" $ do
-#         x `shouldBe` True
-#     describe y" $ do
-#       it "should be False" $ do
-#         x `shouldBe` False
-# HASKELL
-#   end
-#
-#   it { expect(compilation).to include expected_compilation_body }
-#   it { expect(compilation).to include expected_compilation_imports }
-# end
-#
+require_relative './spec_helper'
+
+describe PhpTestHook do
+  let(:hook) { PhpTestHook.new(nil) }
+  let(:compilation) { hook.compile_file_content(treq(sample_content, sample_test, sample_extra)) }
+
+  let(:sample_extra) { '$extra = 2;' }
+  let(:sample_content) { 'class Foo { function bar() { return "baz"; } }' }
+  let(:sample_test) {
+    <<PHP
+public function testFooBarBaz(): void {
+  $this->assertEquals(new Foo().bar(), "baz");
+}
+PHP
+  }
+  let(:expected_compilation) do
+    <<PHP
+<?php
+declare(strict_types=1);
+
+$extra = 2;
+class Foo { function bar() { return "baz"; } }
+
+use PHPUnit\\Framework\\TestCase;
+
+final class AAAMumukiTestCaseAAATest extends TestCase {
+  public function testFooBarBaz(): void {
+    $this->assertEquals(new Foo().bar(), "baz");
+  }
+
+}
+PHP
+  end
+
+  it { expect(compilation).to eq expected_compilation }
+end
+
